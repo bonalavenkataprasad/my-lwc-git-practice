bb3255281f6dc32666892d08db7c24e9705945ed
Prasanna Lakshmi Bonala <bonalavenkataprasad@gmail.com>
2026-02-09 19:25:06 +0530

    Added dataBinding Example 1 And 2 LWC

---
+++ force-app/main/default/lwc/dataBindingExample1/dataBindingExample1.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class DataBindingExample1 extends LightningElement {
+  message ='Hello World';  
+}
---
+++ force-app/main/default/lwc/dataBindingExample2/dataBindingExample2.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class DataBindingExample2 extends LightningElement {
+    name='John Doe';
+    company='ABC Corp';
+    department='Sales';
+    role='Manager';
+}
